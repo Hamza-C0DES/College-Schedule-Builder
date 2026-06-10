5616aa32338591ead2ead61ee1d6a905a644d1b5
Hamza Rana <hamzasr6122@gmail.com>
2026-06-10 13:11:44 -0400

    Impelemented the Inputs

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,29 @@ import './App.css';
 
 export default function MyComponent(){
 
+  const course_attributes = [
+    "Course Name",
+    "Day",
+    "Start",
+    "End",
+  ]
+
 return (
   /* RETURNING JSX  */
   <>
   <div id="main-page-div">
     <h1>College Schedule Builder</h1>
-      <div id="add-course"> 
-        <h2>Add a Course</h2>
+    <h2>Add a Course</h2>
+      <div className="input-row"> 
+        {course_attributes.map((course_attribute) => (
+          <input 
+          key={course_attribute}
+          type="text"
+          placeholder={course_attribute}
+          />
+        ))}
+        <button type="submit">+Add</button>
+
         {/* <h3>Course Name</h3> */}
         {/* <input type="text" 
             name="course-name" 
@@ -19,6 +35,8 @@ return (
         <input type="text" name="date" placeholder="Enter course name"></input>
         <button>Add Course</button> */}
         {/* Implementing a Shortcut*/}
+
+
 
       </div>
   </div>
